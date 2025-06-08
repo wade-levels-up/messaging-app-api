5854464ec7283dbf345678f244612c05c74fe057
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-06-09 08:31:45 +1000

    Store hashed passwords on user sign up

--- src/controllers/signUpController.ts
+++ src/controllers/signUpController.ts
@@ -5,6 +5,7 @@ import { validationResult } from "express-validator";
 import { validateUsername, validateEmail, validatePassword } from '../validators/signUpValidators';
 import { sendVerificationEmail } from '../utils/sendEmail';
 import crypto from 'crypto';
+import bcrypt from "bcryptjs";
 
 interface SignUpBody {
   email: string;
@@ -49,10 +50,11 @@ export const signUp = [
             }
 
             const verificationToken = crypto.randomBytes(32).toString('hex');
+            const hashedPassword: string = await bcrypt.hash(password, 10);
 
             await executeWithPrisma(async (prisma) => {
                 await prisma.user.create({
-                    data: { username, email, password, verificationToken }
+                    data: { username, email, password: hashedPassword, verificationToken }
                 })
             })
 

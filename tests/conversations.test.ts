13d57f35fa2322d9dad8329cfd09c5a71eb5e60d
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-06-22 13:55:08 +1000

    test: User can create conversation with an opening message

--- tests/conversations.test.ts
+++ tests/conversations.test.ts
@@ -1,6 +1,7 @@
 import supertest from "supertest";
 import { testApp } from "./utils/testApp";
 import signInUser from "./utils/signInUser";
+import { handleError } from "../src/utils/handleError";
 
 test("User with access token can retrieve conversations", async () => {
     const signInRes = await signInUser("johndoe@testmail.com", "SuperSecret11")
@@ -91,6 +92,39 @@ test("Retrieve specific conversation's messages for logged in user", async () =>
 
     expect(response.body).toHaveProperty("conversationMessages")
     expect(response.body.conversationMessages[0].content).toBe("Good morning!");
+})
+
+test("User can create a new conversation between themselves and other users with an opening message", async () => {
+    const signInRes = await signInUser("johndoe@testmail.com", "SuperSecret11")
+    const token = signInRes.body.token; 
+    
+    const usernameOfPersonConversationCreatedWith = 'JaneDoe'
+
+    await supertest(testApp)
+        .post('/conversations')
+        .set("Authorization", `Bearer ${token}`)
+        .type("form")
+        .send({ user: usernameOfPersonConversationCreatedWith, openingMessage: 'Hi Jane!' })
+        .expect("Content-Type", /json/)
+        .expect({ message: `New conversation started between JohnDoe and ${usernameOfPersonConversationCreatedWith}` })
+        .expect(201);
+
+    // Retrieve the id of the newly created conversation with Jane
+    const conversationResponse = await supertest(testApp)
+        .get("/conversations")
+        .set("Authorization", `Bearer ${token}`)
+        .expect("Content-Type", /json/)
+        .expect(200);
+    const conversationId = conversationResponse.body.conversations[1].id
+    
+    const response = await supertest(testApp)
+        .get(`/conversations/${conversationId}/messages`)
+        .set("Authorization", `Bearer ${token}`)
+        .expect("Content-Type", /json/)
+        .expect(200);
+
+    expect(response.body).toHaveProperty("conversationMessages")
+    expect(response.body.conversationMessages[0].content).toBe("Hi Jane!");
 })
 
 

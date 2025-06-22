f10a5b9e34231d1ac4c90869c073554efebbdf21
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-06-23 09:56:40 +1000

    feat: Let users add friends to their friends list

--- src/controllers/friendsController.ts
+++ src/controllers/friendsController.ts
@@ -23,3 +23,40 @@ export const getMyFriends = asyncHandler(async (req: Request, res: Response) => 
         handleError(error);
     }
 })
+
+export const addFriend = asyncHandler(async (req: Request, res: Response) => {
+    try {
+        // Collect a list of the logged in user's current friends
+        const user = await prisma.user.findUnique({
+            where: { id: (req as any).userId },
+            include: { friends: true, friendsOf: true },
+        });
+
+        // Check that the friend to be added doesn't already exist in the list
+        user?.friends.forEach((friend) => {
+            if (friend.username === req.params[`username`]) {
+                res.status(404).json({ message: `Cannot update friends list. User: ${req.params[`username`]} is already a friend` })     
+            }
+        })
+
+        // Retrieve the friend to be added to access their ID
+        const recipient = await prisma.user.findUnique({
+            where: { username: req.params[`username`] }
+        });
+
+        if (!recipient) {
+            res.status(404).json({ message: `Cannot update friends list. Unable to retrieve ID of recipient` });
+            return;
+        }
+
+        // If the friend doesn't already exist, update the friend's list to include them
+        await prisma.user.update({
+            where: { id: (req as any).userId },
+            data: { friends: { connect: { id: recipient.id } } }
+        });
+        
+        res.status(201).json({ message: `Succesfully added ${req.params[`username`]} as a friend` })
+    } catch(error) {
+        handleError(error)
+    }
+})
--- src/routes/friends.ts
+++ src/routes/friends.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getMyFriends } from '../controllers/friendsController';
+import { getMyFriends, addFriend } from '../controllers/friendsController';
 import { decodeAndAttachJWT } from '../middleware/decodeAndAttachJWT';
 
 const friendsRouter = Router();
 
 friendsRouter.get("/", decodeAndAttachJWT, getMyFriends);
+friendsRouter.put("/:username", decodeAndAttachJWT, addFriend);
 
 export { friendsRouter };
